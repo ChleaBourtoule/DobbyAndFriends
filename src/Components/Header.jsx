6fd2158f33baba2233922cce023c46bf78dc3d4f
Bastien Traille <b.traille@hotmail.fr>
2021-10-27 16:06:44 +0200

    modified header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,20 @@ import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => (
-    <ul className='Header'>
-        <li><NavLink activeClassName="active" exact to='/'>Home</NavLink></li>
-        <li><NavLink activeClassName="active" to='/'>Library</NavLink></li>
+  <div className="Header">
+    <ul className="NavBar">
+      <li>
+        <NavLink activeClassName="active" exact to="/">
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink activeClassName="active" to="/library">
+          Library
+        </NavLink>
+      </li>
     </ul>
-)
+  </div>
+);
 
 export default Header;
