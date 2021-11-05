bb49425f641a8b73948d8764cf87d578cc976a20
Bastien Traille <b.traille@hotmail.fr>
2021-11-05 23:08:15 +0100

    made some css for QuizStart.jsx

--- src/Components/Pages/Quiz/QuizStart.jsx
+++ src/Components/Pages/Quiz/QuizStart.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './quizStart.css';
+import photoTest from '../../../assets/harryPotterPhotoBomb.jpg';
 
 const QuizStart = () => {
   return (
     <div className="quiz-start">
-      <div className="quiz-presentation">
-        Answer 5 questions and find out how well you know Dobby&apos;s friends
-        <div className="question">
-          question
-          <div className="answers">Answers</div>
-        </div>
+      <div className="quiz-rules">
+        <h2>
+          Answer 5 questions and find out how well you know Dobby&apos;s friends
+        </h2>
+      </div>
+      <div className="img-question-div">
+        <img src={photoTest} alt="" />
+        <p>Can you guess the house this character belongs to ?</p>
+      </div>
+      <div className="answers">
+        <button type="button" className="answer cursor">
+          Answer 1
+        </button>
+        <button type="button" className="answer cursor">
+          Answer 2
+        </button>
+        <button type="button" className="answer cursor">
+          Answer 3
+        </button>
+        <button type="button" className="answer cursor">
+          Answer 4
+        </button>
       </div>
     </div>
   );
