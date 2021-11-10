8b91298515f31330ca70fc2b229747c620f34dbe
Laurent SLONIM <slonim.laurent@gmail.com>
2021-11-10 15:15:05 +0100

    feat: your favorite character

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import QuizStart from './Components/Pages/Quiz/QuizStart';
+import Favorite from './Components/Pages/Favorite';
 import Quiz from './Components/Pages/Quiz/Quiz';
 import Header from './Components/Header';
 import Library from './Components/Pages/Library';
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/quiz" component={Quiz} />
           <Route exact path="/library" component={Library} />
           <Route exact path="/quiz-start" component={QuizStart} />
+          <Route path="/favorite" component={Favorite} />
         </Switch>
       </HashRouter>
     </div>
