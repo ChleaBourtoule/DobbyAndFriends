64ec4647cfe4548ac45b5667bb1231c13015b7ae
Bastien Traille <b.traille@hotmail.fr>
2021-11-11 18:06:23 +0100

    some css updates with logo on answers

--- src/Components/Pages/Quiz/QuizStart.jsx
+++ src/Components/Pages/Quiz/QuizStart.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './quizStart.css';
 import photoTest from '../../../assets/harryPotterPhotoBomb.jpg';
+import gryffindorLogo from '../../../assets/gryffindor.png';
+import ravenclawLogo from '../../../assets/ravenclaw.png';
+import hufflepuffLogo from '../../../assets/hufflepuff.png';
+import slytherinLogo from '../../../assets/slytherin.png';
 
 const QuizStart = () => {
   const [characters, setCharacters] = useState([{}]);
@@ -19,11 +23,13 @@ const QuizStart = () => {
       .then((res) => res.data)
       .then((data) => setCharacters(data));
     console.log(characters);
+  }, []);
+
+  useEffect(() => {
     setCharacters(
-      characters.filter(
-        (character) => character.house !== null && character.image !== null
-      )
+      characters.filter((character) => character.house && character.image)
     );
+    console.log(characters, `Console log useEffect`);
   }, []);
 
   useEffect(() => {
@@ -52,47 +58,85 @@ const QuizStart = () => {
           Answer 5 questions and find out how well you know Dobby&apos;s friends
         </h2>
       </div>
-      <div className="img-question-div">
-        <img src={characterImg} alt="" />
-        <p>Can you guess the house {characterName} belongs to ?</p>
-      </div>
-      <div className="answers">
-        <button
-          type="button"
-          id="button-1"
-          value="Gryffindor"
-          className={trueAnswer ? 'answer cursor true-answer' : 'answer cursor'}
-          onClick={(e) => isItRightAnswer(e.target.value)}
-        >
-          Gryffindor
-        </button>
-        <button
-          type="button"
-          id="button-2"
-          value="Ravenclaw"
-          className="answer cursor"
-          onClick={(e) => isItRightAnswer(e.target.value)}
-        >
-          Ravenclaw
-        </button>
-        <button
-          type="button"
-          id="button-3"
-          value="Hufflepuff"
-          className="answer cursor"
-          onClick={(e) => isItRightAnswer(e.target.value)}
-        >
-          Hufflepuff
-        </button>
-        <button
-          type="button"
-          id="button-4"
-          value="Slytherin"
-          className="answer cursor"
-          onClick={(e) => isItRightAnswer(e.target.value)}
-        >
-          Slytherin
-        </button>
+
+      <div className="img-question-answers">
+        <div className="img">
+          <img src={!characterImg ? photoTest : characterImg} alt="" />
+        </div>
+
+        <div className="question-answers">
+          <div className="question">
+            <p>
+              Can you guess the house{' '}
+              {!characterName ? 'this character' : characterName} belongs to ?
+            </p>
+          </div>
+
+          <div className="answers">
+            <button
+              type="button"
+              id="button-1"
+              value="Gryffindor"
+              className={
+                trueAnswer ? 'answer cursor true-answer' : 'answer cursor'
+              }
+              onClick={(e) => isItRightAnswer(e.target.value)}
+            >
+              Gryffindor
+              <img
+                className="house"
+                id="gryffindorImg"
+                src={gryffindorLogo}
+                alt="hufflepuff logo"
+              />
+            </button>
+            <button
+              type="button"
+              id="button-2"
+              value="Ravenclaw"
+              className="answer cursor"
+              onClick={(e) => isItRightAnswer(e.target.value)}
+            >
+              Ravenclaw
+              <img
+                className="house"
+                id="ravenclawImg"
+                src={ravenclawLogo}
+                alt="ravenclaw logo"
+              />
+            </button>
+            <button
+              type="button"
+              id="button-3"
+              value="Hufflepuff"
+              className="answer cursor"
+              onClick={(e) => isItRightAnswer(e.target.value)}
+            >
+              Hufflepuff
+              <img
+                className="house"
+                id="hufflepuffImg"
+                src={hufflepuffLogo}
+                alt="hufflepuff logo"
+              />
+            </button>
+            <button
+              type="button"
+              id="button-4"
+              value="Slytherin"
+              className="answer cursor"
+              onClick={(e) => isItRightAnswer(e.target.value)}
+            >
+              Slytherin
+              <img
+                className="house"
+                id="slytherinImg"
+                src={slytherinLogo}
+                alt="slytherin logo"
+              />
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
