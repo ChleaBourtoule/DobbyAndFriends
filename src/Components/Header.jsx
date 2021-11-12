054f4a57bf9405d306701721d567504d11ce9967
Bastien Traille <b.traille@hotmail.fr>
2021-11-12 11:19:07 +0100

    added arrows on Quiz page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,56 +3,58 @@ import './header.css';
 import logoNavbar from '../assets/logo-navbar.png';
 import solemnlySwear from '../assets/solemnly-swear.png';
 
-const Header = () => (
-  <div className="header">
-    <div>
-      <NavLink className="nav-link" exact to="/">
-        <img className="cursor" src={logoNavbar} alt="Dobby" />
-      </NavLink>
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <NavLink className="nav-link" exact to="/">
+          <img className="cursor" src={logoNavbar} alt="Dobby" />
+        </NavLink>
+      </div>
+      <div>
+        <img src={solemnlySwear} alt="I solemnly swear..." />
+      </div>
+      <ul className="nav-bar">
+        <li>
+          <NavLink
+            className="cursor nav-link"
+            activeClassName="active"
+            exact
+            to="/"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="cursor nav-link"
+            activeClassName="active"
+            to="/quiz"
+          >
+            Quiz
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="cursor nav-link"
+            activeClassName="active"
+            to="/library"
+          >
+            Library
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className="cursor nav-link"
+            activeClassName="active"
+            to="/favorite"
+          >
+            Your Character
+          </NavLink>
+        </li>
+      </ul>
     </div>
-    <div>
-      <img src={solemnlySwear} alt="I solemnly swear..." />
-    </div>
-    <ul className="nav-bar">
-      <li>
-        <NavLink
-          className="cursor nav-link"
-          activeClassName="active"
-          exact
-          to="/"
-        >
-          Home
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="cursor nav-link"
-          activeClassName="active"
-          to="/quiz"
-        >
-          Quiz
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="cursor nav-link"
-          activeClassName="active"
-          to="/library"
-        >
-          Library
-        </NavLink>
-      </li>
-      <li>
-        <NavLink
-          className="cursor nav-link"
-          activeClassName="active"
-          to="/favorite"
-        >
-          Your Character
-        </NavLink>
-      </li>
-    </ul>
-  </div>
-);
+  );
+};
 
 export default Header;
