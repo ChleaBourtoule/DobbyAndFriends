8680a0884354ab91a66fcc40fb15eb5061010ea8
Just4Joy <slonim.laurent@gmail.com>
2021-11-15 12:18:29 +0100

    Revert "Dev"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { HashRouter, Switch, Route } from 'react-router-dom';
-import QuizStart from './Components/Pages/Quiz/QuizStart';
-import Favorite from './Components/Pages/Favorite';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Quiz from './Components/Pages/Quiz/Quiz';
 import Header from './Components/Header';
 import Library from './Components/Pages/Library';
@@ -12,16 +10,15 @@ import './app.css';
 function App() {
   return (
     <div>
-      <HashRouter basename="/">
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/quiz" component={Quiz} />
-          <Route exact path="/library" component={Library} />
-          <Route exact path="/quiz-start" component={QuizStart} />
-          <Route path="/favorite" component={Favorite} />
-        </Switch>
-      </HashRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/quiz" component={Quiz} />
+        <Route path="/library" component={Library} />
+        <Route path="*">
+          <Redirect exact to="/" />
+        </Route>
+      </Switch>
     </div>
   );
 }
