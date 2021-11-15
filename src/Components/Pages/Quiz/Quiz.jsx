b7c86a5302f3c5f9e82ea6ca656553126f4f1b9b
Bastien Traille <b.traille@hotmail.fr>
2021-11-15 15:53:38 +0100

    started responsive on quiz.css

--- src/Components/Pages/Quiz/Quiz.jsx
+++ src/Components/Pages/Quiz/Quiz.jsx
@@ -5,8 +5,8 @@ import '../../../arrows.css';
 const Quiz = () => {
   return (
     <div className="quiz">
-      <h1 className="title quiz-title">Do you know Dobby&apos; friends ?</h1>
-      <div>
+      <h1 className="title quiz-title">Do you know Dobby&apos;s friends ?</h1>
+      <div className="arrows-div">
         <svg className="arrows">
           <path className="a1" d="M0 0 L30 32 L60 0" />
           <path className="a2" d="M0 20 L30 52 L60 20" />
