005036f38fea3f569579dc09420322558d5191ae
amandine <constans.amandine@gmail.com>
2021-11-18 12:24:07 +0100

    final responsive arrow quiz

--- src/Components/Pages/Quiz/Quiz.jsx
+++ src/Components/Pages/Quiz/Quiz.jsx
@@ -8,18 +8,18 @@ const Quiz = () => {
       <h1 className="title-bis" id="quiz-title">
         Do you know Dobby&apos;s friends ?
       </h1>
-      <div>
+      <div className="container-arrows">
         <svg className="arrows">
           <path className="a1" d="M0 0 L30 32 L60 0" />
           <path className="a2" d="M0 20 L30 52 L60 20" />
           <path className="a3" d="M0 40 L30 72 L60 40" />
         </svg>
+        <HashLink className="nav-link" exact to="/quiz-start#root">
+          <button type="button" id="start-quiz" className="cursor">
+            Enter into the Chamber of Secrets
+          </button>
+        </HashLink>
       </div>
-      <HashLink className="nav-link" exact to="/quiz-start#root">
-        <button type="button" id="start-quiz" className="cursor">
-          Enter into the Chamber of Secrets
-        </button>
-      </HashLink>
     </div>
   );
 };
